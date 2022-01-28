e889e90f8e541c76dae6979638e6c004fc8e0073
George <george.yarwood@gmail.com>
2022-01-28 23:25:05 +0000

    Pickup script

--- VladNFriends Game/Assets/Scripts/George's shiz/Pickup.cs
+++ VladNFriends Game/Assets/Scripts/George's shiz/Pickup.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Pickup : MonoBehaviour
 {
+    //Where items will go when picked up
+    public Transform pickUpPos;
+
+    //Show when hovering over item we can pickup
+    public GameObject pickUpTxt;
+
+    //Gameobject we're currently holding
+    GameObject currHeldObj;
+
+    bool holdingItem = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,12 +24,53 @@ public class Pickup : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0)) 
+    void FixedUpdate()
+    {                
+        //Cast ray from camera
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out hit, 10f))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        }        
+            //If the item can be picked up
+            if (hit.collider.tag == "pickup")
+            {
+                if (!holdingItem) 
+                {
+                    //Show UI
+                    pickUpTxt.SetActive(true);
+                }
+                
+                if (Mouse.current.leftButton.wasPressedThisFrame)
+                {
+                    currHeldObj = hit.collider.gameObject;
+                    holdingItem = false;
+
+                    //Move and parent
+                    currHeldObj.transform.position = pickUpPos.transform.position;
+                    currHeldObj.GetComponent<Rigidbody>().isKinematic = true;
+                    currHeldObj.transform.parent = pickUpPos;
+                    holdingItem = true;
+                    //Show UI
+                    pickUpTxt.SetActive(false);
+                }
+
+            }
+            else 
+            {
+                pickUpTxt.SetActive(false);
+            }
+            if(Mouse.current.leftButton.wasReleasedThisFrame && holdingItem)
+            {
+                //Release
+                currHeldObj.GetComponent<Rigidbody>().isKinematic = false;
+                currHeldObj.transform.parent = null;
+                holdingItem = false;
+            }
+
+           
+
+        }
+           
     }
 }
