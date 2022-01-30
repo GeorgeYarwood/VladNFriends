fafb294652ca6fec8e0aae8a41430298492ac8fa
Vlad Savenco (savencov) <savencov@coventry.ac.uk>
2022-01-30 01:12:24 +0000

    sdfg

--- VladNFriends Game/Assets/Enemy/EnemyAi.cs
+++ VladNFriends Game/Assets/Enemy/EnemyAi.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyAi : MonoBehaviour
 {
@@ -21,7 +22,7 @@ public class EnemyAi : MonoBehaviour
 
     private void Update()
     {
-        if (Vector3.Distance(target.transform.position, gameObject.transform.position) > 4.0f)
+        if (Vector3.Distance(target.transform.position, gameObject.transform.position) > 1.5f)
         {
             agent.SetDestination(target.position);
 
@@ -32,6 +33,11 @@ public class EnemyAi : MonoBehaviour
             anim.SetBool("IsWalking", false);
 
             agent.SetDestination(transform.position);
+
+            Debug.Log("i catch");
+
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
         }
     }
 }
