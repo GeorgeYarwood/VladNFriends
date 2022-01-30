e81310f4780c4ff9ade1a74bdb4bca953ffb0c2c
George <george.yarwood@gmail.com>
2022-01-30 15:24:51 +0000

    Fist level done also fix

--- VladNFriends Game/Assets/Scripts/George's shiz/PlayerController.cs
+++ VladNFriends Game/Assets/Scripts/George's shiz/PlayerController.cs
@@ -33,8 +33,8 @@ public class PlayerController : MonoBehaviour
 
         //Hide and lock cursor
         Cursor.lockState = CursorLockMode.Locked;
-        //keyMoveEnabled = true;
-        //mouseMoveEnabled = true; 
+        keyMoveEnabled = true;
+        mouseMoveEnabled = true; 
         canJump = true;
     }
 
